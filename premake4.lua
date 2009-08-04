ac2389b083414b385e9ba0def627ab14d3150208
jkroll <jkroll@a8b4d248-f79a-41f0-bb0f-d2329e5d8193>
2009-08-04 12:40:10 +0000

    git-svn-id: svn+ssh://svn.tuxfamily.org/svnroot/tumbetoene/wolpertinger/trunk@11 a8b4d248-f79a-41f0-bb0f-d2329e5d8193

---
+++ premake4.lua
@@ -0,0 +1,68 @@
+solution "Wolpertinger"
+	includedirs {
+		"../juced/juce",	-- adjust this to match your juce basedir containing the 'src' subdirectory
+		"../vstsdk2.4", 	-- adjust this to match your vst sdk 2.4 basedir containing the 'public.sdk' subdirectory
+		"src"
+	}
+
+	libdirs {
+		"../juced/bin", -- adjust this to match your juce bindir
+		"/usr/X11R6/lib",
+		"/usr/lib" }
+
+	configurations { "Debug", "Release" }
+	objdir "build"
+	targetname "Wolpertinger"
+	targetprefix ""
+
+	defines { "LINUX=1", "JUCE_USE_XSHM=1", "JUCE_ALSA=1", "JUCE_USE_VSTSDK_2_4=1" }
+
+	files { "src/**.cpp", "src/**.h" }
+
+	links { "freetype", "pthread", "rt", "X11",
+		"GL", "GLU", "Xinerama", "asound", "m" }
+
+
+
+
+project "WolpertingerVST"
+	kind "SharedLib"
+	language "C++"
+	files { "vst/**.cpp", "vst/**.h" }
+
+configuration "Debug"
+	defines { "DEBUG=1", "_DEBUG=1" }
+	links { "juce_debug" }
+	flags { "Symbols" }
+	buildoptions { "-ggdb" }
+	targetdir "build/Debug"
+
+configuration "Release"
+	defines { "NDEBUG=1" }
+	links { "juce" }
+	flags { "Optimize" }
+	buildoptions { "-O2" }
+	targetdir "build/Release"
+
+
+
+
+project "WolpertingerStandalone"
+	kind "WindowedApp"
+	language "C++"
+	files { "standalone/**.cpp", "standalone/**.h" }
+
+configuration "Debug"
+	defines { "DEBUG=1", "_DEBUG=1" }
+	links { "juce_debug" }
+	flags { "Symbols" }
+	buildoptions { "-ggdb" }
+	targetdir "build/Debug"
+
+configuration "Release"
+	defines { "NDEBUG=1" }
+	links { "juce" }
+	flags { "Optimize" }
+	buildoptions { "-O2" }
+	targetdir "build/Release"
+
